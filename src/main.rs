856767a28ee25a2d364bf2c49e5a365408219fbd
Gittoman <tomvar98@gmail.com>
2024-01-28 21:57:45 +0200

    christmas carol

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,67 @@ use std::{
     string,
 };
 
-fn main() {}
+fn main() {
+    christmas();
+}
+
+fn christmas() {
+    let days = [
+        "On the second day of Christmas,",
+        "On the third day of Christmas,",
+        "On the fourth day of Christmas,",
+        "On the fifth day of Christmas,",
+        "On the sixth day of Christmas,",
+        "On the seventh day of Christmas,",
+        "On the eighth day of Christmas,",
+        "On the ninth day of Christmas,",
+        "On the tenth day of Christmas,",
+        "On the eleventh day of Christmas,",
+        "On the twelfth day of Christmas,",
+    ];
+    let mut lyrics = [
+        "Twelve drummers drumming,",
+        "Eleven pipers piping,",
+        "Ten lords a-leaping,",
+        "Nine ladies dancing,",
+        "Eight maids a-milking,",
+        "Seven swans a-swimming,",
+        "Six geese a-laying,",
+        "Five golden rings,",
+        "Four calling birds,",
+        "Three French hens,",
+        "Two turtle doves,",
+        "And a partridge in a pear tree!",
+    ];
+    lyrics.reverse();
+
+    let mut counter = 0;
+    println!("On the first day of Christmas,");
+    println!("my true love gave to me",);
+    println!("A partridge in a pear tree");
+    println!("");
+
+    for index in days {
+        println!("{index}");
+        println!("my true love gave to me",);
+        counter = counter + 1;
+        for i in (0..counter + 1).rev() {
+            println!("{}", lyrics[i]);
+        }
+        println!("");
+    }
+}
+
+// 0 1 1 2 3 5 8 13 21 34
+fn fibonacci(n: i32) -> i32 {
+    if n <= 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
 
 fn temperature_converter() {
     println!("This is a temperature converter!");
