b776841cfe790d495d056e4279696295350c33d8
Gittoman <tomvar98@gmail.com>
2024-01-31 17:30:56 +0200

    started the practise question

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,10 @@
-use std::collections::HashMap;
-
 // implementation of rust tutorial practise question
 
-// Given a list of integers, use a vector and
-// return the median (when sorted, the value in
-// the middle position) and mode (the value that
-// occurs most often; a hash map will be helpful here) of the list.
+// Using a hash map and vectors, create a text interface
+// to allow a user to add employee names to a department
+// in a company. For example, “Add Sally to Engineering”
+// or “Add Amir to Sales.” Then let the user retrieve a
+// list of all people in a department or all people in the
+// company by department, sorted alphabetically.
 
-fn main() {
-    let mut numbers = vec![1, 2, 3, 5, 4, 5, 1, 2, 3, 4, 5];
-    let result = median(&mut numbers);
-    println!("Median of the list is: {result}");
-    // println!("{:?}", numbers);
-    println!("Mode of the list is: {}", mode(numbers));
-}
-
-fn median(list: &mut Vec<i32>) -> i32 {
-    // given a list of integers return median
-    if list.is_empty() {
-        return 0;
-    }
-    // println!("{:?}", list);
-    list.sort();
-    // println!("{:?}", list);
-    match list.get(list.len() / 2) {
-        Some(value) => return *value,
-        None => return 0,
-    }
-}
-
-fn mode(list: Vec<i32>) -> i32 {
-    //given a list of integers return mode of the list
-    if list.is_empty() {
-        return 0;
-    }
-
-    // calculate hashmap for all the values to get frequency of the numbers
-    let mut map: HashMap<i32, i32> = HashMap::new();
-    for value in list {
-        let count = map.entry(value).or_insert(0);
-        *count += 1;
-    }
-
-    // get biggest value from the map values that include the frequency
-    let mut max_key = 0;
-    let mut biggest = 0;
-    for (key, value) in &map {
-        if value > &biggest {
-            biggest = *value;
-            max_key = *key;
-        }
-    }
-    // println!("{:?}", map);
-    max_key
-    // if let Some(value) = map.get(&max_key) {
-    //     return *value;
-    // } else {
-    // 0
-    // }
-}
+fn main() {}
