99b7f7d2bfa1a1aae505d5f41021b4b89531a98c
Gittoman <tomvar98@gmail.com>
2024-02-03 16:23:25 +0200

    generics part of the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
+// generics allow us to replace specific types with a
+// placeholder that represents multiple types without
+// code dublication
+
 fn main() {
-    let message = "Hello world!";
+    let number_list = vec![10, 20, 30, 40];
+    let char_list = vec!['a', 'v', 'b', 'i', 'c'];
 
-    println!("{message}");
+    let point = Point { x: 5, y: 10 };
+    let pointf = Point { x: 5.5, y: 10.5 };
 
-    let message = "This is the main branch\n\
-        Check other branches for different practise projects\n";
+    let largest = find_largest(&number_list);
+    let largest2 = find_largest_generic(&char_list);
 
-    print!("{message}");
+    println!("The largest number is {} - {}", largest, largest2);
+}
+
+fn find_largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+// some T values doesn't work with compare operator >
+// std::cmp::partialOrd is used here since it allows
+// T to be only char or int
+fn find_largest_generic<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+// T can only be a single type when defining a struct Point
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+// using T & U allows us to use eq. int and float when defining a Point_
+struct Point_<T, U> {
+    x: T,
+    y: U,
 }
